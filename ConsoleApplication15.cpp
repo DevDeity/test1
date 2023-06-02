078304fffddc890c4d32e7ed2c65819da1576a80
DevDeity <131965475+DevDeity@users.noreply.github.com>
2023-06-02 18:02:10 +0300

    Add files via upload

---
+++ ConsoleApplication15.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+ 
+int main()
+{
+	setlocale(0, "Russian");
+	int value;
+	cout << "Введите число" << endl;
+	cin >> value;
+
+	cout << "Число " << value << " является " << (value % 2 == 0 ? "чётным" : "нечётным") << endl;
+}
